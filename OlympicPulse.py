ace4c465ba63fb2e84bf674647df05e03755d068
Suryansh Singh <160277989+singhsuryanshofficial@users.noreply.github.com>
2024-04-07 03:22:30 +0530

    Update OlympicPulse.py

--- OlympicPulse.py
+++ OlympicPulse.py
@@ -99,12 +99,15 @@ if user_menu == 'Overall Analysis':
         st.title(athletes)
 
 #--------------------------Line Chart-> Participating Nations Over Time-------------------------------------------------
+    st.title("Overall Athlete Data")
     st.dataframe(df)
+
+    st.title("Participating nations over the years")
     nations_over_time = helper.data_over_time(df, 'Region')
     
     #st.dataframe(nations_over_time)
     fig = px.line(nations_over_time, x="Edition", y="Region")
-    st.title("Participating nations over the years")
+    
     st.plotly_chart(fig)
 
 #--------------------------Line Chart-> No. of Events Over the Years----------------------------------------------------
