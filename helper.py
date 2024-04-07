90a1cf1252a821bf2f50c0effb6fc572b19cd274
Suryansh Singh <160277989+singhsuryanshofficial@users.noreply.github.com>
2024-04-07 12:02:12 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -92,20 +92,15 @@ def athletes_over_time(df):
 
 
 def most_successful(df, sport):
-    temp_df = df.dropna(subset=['Medal']) # Drop rows where 'Medal' is NaN
+    temp_df = df.dropna(subset=['Medal']) # eliminate nan values of medal
     if sport != 'Overall':
-        temp_df = temp_df[temp_df['Sport'] == sport]  # Filter by sport if specified
-        print(temp_df)
-    # Count the number of medals for each athlete, select the top 15, and merge with the original DataFrame
-    x = temp_df['Name'].value_counts().reset_index().head(15) #.merge(df, left_index=True, right_on='Name', how='left')
-    
-    # Select desired columns and drop duplicates based on athlete's name
-    x = x[['Name_x', 'Sport', 'Region']].drop_duplicates('Name_x')
-    
-    # Rename columns for clarity
-    x.rename(columns={'Name_x': 'Name'}, inplace=True)
-    
+        temp_df = temp_df[temp_df['Sport'] == sport]  # removed all other rows (except input sport)
+
+    x = temp_df['Name'].value_counts().reset_index().head(15).merge(df, left_on='index', right_on='Name', how='left')[
+        ['index', 'Name_x', 'Sport', 'Region']].drop_duplicates('index')
+    x.rename(columns={'index': 'Name', 'Name_x': 'Medals'}, inplace=True)
     return x
+
 
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #-----------------------------------------------------------------------------COUNTRY WISE ANALYSIS SECTION FUNCTIONS--------------------------------------------------------------------------------------------------------------
