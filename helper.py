e1a596306093606c00ac4be3d3ecc8cc970f630f
Suryansh Singh <160277989+singhsuryanshofficial@users.noreply.github.com>
2024-04-07 11:45:58 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -98,10 +98,14 @@ def most_successful(df, sport):
         
     #x = temp_df['Name'].value_counts().reset_index().head(15).merge(df, left_on='index', right_on='Name', how='left')[['index', 'Name_x', 'Sport', 'Region']].drop_duplicates('index')
 
-    x = temp_df['Name'].value_counts().reset_index().head(15).merge(df, left_index=True, right_on='Name', how='left')[['index', 'Name_x', 'Sport', 'Region']].drop_duplicates('index')
+    #x = temp_df['Name'].value_counts().reset_index().head(15).merge(df, left_index=True, right_on='Name', how='left')[['index', 'Name_x', 'Sport', 'Region']].drop_duplicates('index')
 
-
+    x = temp_df['Name'].value_counts().reset_index().head(15).merge(df, left_index=True, right_on='Name', how='left').drop_duplicates('index')
     x.rename(columns={'index': 'Name', 'Name_x': 'Medals'}, inplace=True)
+
+
+
+    #x.rename(columns={'index': 'Name', 'Name_x': 'Medals'}, inplace=True)
     return temp_df
 
 
